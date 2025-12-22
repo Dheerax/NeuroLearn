21e57e3c681b577c5d2b07567001c48024e38417
Dheerax <dheeraxgamerz@gmail.com>
2025-12-22 21:07:12 +0530

    error

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import { FocusMonitorProvider } from "./context/FocusMonitorContext";
 import { FocusProvider } from "./context/FocusContext";
 import Layout from "./components/Layout/Layout";
 import DistractionAlert from "./components/DistractionAlert";
-import AICompanion from "./components/AICompanion";
+import AICompanions from "./components/AICompanion";
 import Dashboard from "./pages/Dashboard";
 import TasksPage from "./pages/TasksPage";
 import LearningPage from "./pages/LearningPage";
